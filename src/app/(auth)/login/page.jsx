a30fff39d43236cd6759f14c146f55c0fc991b04
Tamim379ta <tamimthasan55@gmail.com>
2026-05-01 23:56:05 +0600

    edit login and register page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -5,68 +5,68 @@ import Link from "next/link";
 
 const LoginPage = () => {
   return (
-    <div className=' flex flex-col md:flex-row gap-10  container bg-linear-to-r from-cyan-400 via-blue-900 to-[#020024] mx-auto my-10 p-10 rounded-2xl'>
+    <div className="p-5 md:p-0">
+      <div className=' flex flex-col md:flex-row gap-10  container bg-linear-to-r from-cyan-400 via-blue-900 to-[#020024] mx-auto my-10 p-10 rounded-2xl'>
 
-      <div className='p-0 md:p-10 '>
+        <div className='p-0 md:p-10 '>
 
-        <p className='text-2xl font-bold'>Welcome Back!</p>
-        <div className="hidden md:block">
-          <p>to,
-            <span className='text-4xl font-bold'>QurbaniHat – Livestock <br /> Booking Platform</span>
-          </p>
-          <p className="mt-2">A modern livestock marketplace where users can explore <br /> animals for Qurbani such as cows and goats.</p>
+          <p className='text-2xl font-bold'>Welcome Back!</p>
+          <div className="hidden md:block">
+            <p>to,
+              <span className='text-4xl font-bold'>QurbaniHat – Livestock <br /> Booking Platform</span>
+            </p>
+            <p className="mt-2">A modern livestock marketplace where users can explore <br /> animals for Qurbani such as cows and goats.</p>
 
-          <div className="mt-10 text-6xl opacity-50">
-            🐄 🐐 🐑
+            <div className="mt-10 text-6xl opacity-50">
+              🐄 🐐 🐑
+            </div>
           </div>
+
         </div>
 
-      </div>
+        <div className='p-0 md:p-10'>
 
-      <div className='p-0 md:p-10'>
+          <div className="boder bg-blue-950 p-10 md:p-15 rounded-2xl">
+            <form>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend text-white">Email</legend>
+                <input
+                  type="email"
+                  required
+                  className="text-white border-b border-gray-400  p-2.5 outline-none "
+                  placeholder="type here" />
+              </fieldset>
 
-        <div className="boder bg-blue-950 p-10 md:p-15 rounded-2xl">
-          <form>
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend text-white">Email</legend>
-              <input
-                type="email"
-                required
-                className="text-white border-b border-gray-400  p-2.5 outline-none "
-                placeholder="type here" />
-            </fieldset>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend text-white">Password</legend>
+                <input
+                  type="text"
+                  required
+                  className="text-white   border-b border-gray-400  p-2.5 outline-none "
+                  placeholder="type here" />
+              </fieldset>
 
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend text-white">Password</legend>
-              <input
-                type="text"
-                required
-                className="text-white   border-b border-gray-400  p-2.5 outline-none "
-                placeholder="type here" />
-            </fieldset>
+              <div className="flex justify-between mt-3">
+                <div></div>
 
-            <div className="flex justify-between mt-3">
-              <div></div>
+                <button className="btn ">Login</button>
+              </div>
 
-              <button className="btn ">Login</button>
+
+            </form>
+
+            <div className="mt-2">
+              <p className="text-white">Dont have any account?
+                <Link className="text-blue-500 ml-2 underline" href={'/register'}>Register</Link>
+              </p>
             </div>
 
-
-          </form>
-
-          <div className="mt-2">
-            <p className="text-white">Dont have any account?
-              <Link className="text-blue-500 ml-2 underline" href={'/register'}>Register</Link>
-            </p>
           </div>
+
 
         </div>
 
-
       </div>
-
-
-
     </div>
   );
 };
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -2,85 +2,89 @@ import Link from "next/link";
 
 const RegisterPAge = () => {
   return (
-    <div className=' flex flex-col md:flex-row gap-10  container bg-linear-to-r from-cyan-400 via-blue-900 to-[#020024] mx-auto my-10 p-10 rounded-2xl'>
+    <div className="p-5 md:p-0">
 
-      <div className='p-0 md:p-10 '>
+      <div className=' flex flex-col md:flex-row gap-10  container bg-linear-to-r from-cyan-400 via-blue-900 to-[#020024] mx-auto my-10 p-10 rounded-2xl'>
 
-        <p className='text-2xl font-bold'>Welcome to,</p>
-        <div className="hidden md:block">
-          <p>
-            <span className='text-4xl font-bold'>QurbaniHat – Livestock <br /> Booking Platform</span>
-          </p>
-          <p className="mt-2">A modern livestock marketplace where users can explore <br /> animals for Qurbani such as cows and goats.</p>
+        <div className='p-0 md:p-10 '>
 
-          <div className="mt-10 text-6xl opacity-50">
-            🐄 🐐 🐑
+          <p className='text-2xl font-bold'>Welcome to, <span className="md:hidden">QurbaniHat</span></p>
+          <div className="hidden md:block">
+            <p>
+              <span className='text-4xl font-bold'>QurbaniHat – Livestock <br /> Booking Platform</span>
+            </p>
+            <p className="mt-2">A modern livestock marketplace where users can explore <br /> animals for Qurbani such as cows and goats.</p>
+
+            <div className="mt-10 text-6xl opacity-50">
+              🐄 🐐 🐑
+            </div>
           </div>
+
         </div>
 
-      </div>
+        <div className='p-0 md:p-10'>
 
-      <div className='p-0 md:p-10'>
+          <div className="boder bg-blue-950 p-10 md:p-15 rounded-2xl">
+            <form>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend text-white">Full Name</legend>
+                <input
+                  type="name"
+                  required
+                  className="text-white border-b border-gray-400  p-2.5 outline-none "
+                  placeholder="name" />
+              </fieldset>
 
-        <div className="boder bg-blue-950 p-10 md:p-15 rounded-2xl">
-          <form>
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend text-white">Full Name</legend>
-              <input
-                type="name"
-                required
-                className="text-white border-b border-gray-400  p-2.5 outline-none "
-                placeholder="name" />
-            </fieldset>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend text-white">Image Url</legend>
+                <input
+                  type="img"
+                  required
+                  className="text-white border-b border-gray-400  p-2.5 outline-none "
+                  placeholder="image url" />
+              </fieldset>
 
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend text-white">Image Url</legend>
-              <input
-                type="img"
-                required
-                className="text-white border-b border-gray-400  p-2.5 outline-none "
-                placeholder="image url" />
-            </fieldset>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend text-white">Email</legend>
+                <input
+                  type="email"
+                  required
+                  className="text-white border-b border-gray-400  p-2.5 outline-none "
+                  placeholder="type here" />
+              </fieldset>
 
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend text-white">Email</legend>
-              <input
-                type="email"
-                required
-                className="text-white border-b border-gray-400  p-2.5 outline-none "
-                placeholder="type here" />
-            </fieldset>
+              <fieldset className="fieldset">
+                <legend className="fieldset-legend text-white">Password</legend>
+                <input
+                  type="text"
+                  required
+                  className="text-white   border-b border-gray-400  p-2.5 outline-none "
+                  placeholder="type here" />
+              </fieldset>
 
-            <fieldset className="fieldset">
-              <legend className="fieldset-legend text-white">Password</legend>
-              <input
-                type="text"
-                required
-                className="text-white   border-b border-gray-400  p-2.5 outline-none "
-                placeholder="type here" />
-            </fieldset>
+              <div className="flex justify-between mt-3">
+                <div></div>
 
-            <div className="flex justify-between mt-3">
-              <div></div>
+                <button className="btn ">Register</button>
+              </div>
 
-              <button className="btn ">Register</button>
+
+            </form>
+
+            <div className="mt-2">
+              <p className="text-white">Already have an account?
+                <Link className="text-blue-500 ml-2 underline" href={'/login'}>Login</Link>
+              </p>
             </div>
 
-
-          </form>
-
-          <div className="mt-2">
-            <p className="text-white">Already have an account?
-              <Link className="text-blue-500 ml-2 underline" href={'/login'}>Login</Link>
-            </p>
           </div>
+
 
         </div>
 
 
+
       </div>
-
-
 
     </div>
   );
